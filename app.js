6753a69cb87be78f9860476d251dd96e9b685e56
Erick Aguilar <aguilar505088@gmail.com>
2021-10-17 14:22:49 -0500

    Agregar pequenio cambio de disenio

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var express = require('express');
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
 var port = process.env.PORT || 3000
 
 var app = express();
- 
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 app.use(express.static('assets'));
- 
+
 app.use('/assets', express.static(__dirname + '/assets'));
 
 app.get('/', function (req, res) {
@@ -17,6 +17,12 @@ app.get('/', function (req, res) {
 
 app.get('/detail', function (req, res) {
     res.render('detail', req.query);
+});
+
+app.post('/checkout', function (req, res) {
+    console.log(req);
+    res.render('home');
+    // res.render('detail', req.query);
 });
 
 app.listen(port);
